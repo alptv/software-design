2148e82cd0f12fabc1411538e737ca95ef4b8cd7
Alexander Laptev <alex.laptev2000@yandex.ru>
2021-10-23 14:10:24 +0300

    Add database url to constructors of servlets

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/MainTest.java
@@ -0,0 +1,7 @@
+package ru.akirakozov.sd.refactoring;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+}
---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServletTest.java
@@ -0,0 +1,6 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import static org.junit.Assert.*;
+public class GetProductsServletTest {
+  
+}
