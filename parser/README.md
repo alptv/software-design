2b03c8927ab4e1c4fc8cd36b89c2f7f2ea2012f9
Alexander Laptev <alex.laptev2000@yandex.ru>
2021-12-12 18:39:35 +0300

    Small fix and run with gradle

---
+++ parser/README.md
@@ -0,0 +1,4 @@
+# Software design
+## 1. LRU-Cache
+* Запуск ` ./gradlew run -q --console=plain`
+* После ввести арифметическое выражение на 1 строке
--- parser/src/main/kotlin/arithmetic/visitor/parser/state/OpenBrace.kt
+++ parser/src/main/kotlin/arithmetic/visitor/parser/state/OpenBrace.kt
@@ -1,12 +1,13 @@
 package arithmetic.visitor.parser.state
 
 import arithmetic.tokenizer.token.Num
+import arithmetic.tokenizer.token.OpenBracket
 import arithmetic.tokenizer.token.Token
 
 class OpenBrace : BaseState(), ValidationState {
 
     override fun nextState(token: Token): ValidationState {
-        if (token !is Num) {
+        if (token !is Num && token !is OpenBracket) {
             throw IllegalStateException("After open bracket can be only number")
         }
         return super.nextState(token)
